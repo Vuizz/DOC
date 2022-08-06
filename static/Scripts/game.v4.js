493405cbef1639b012c7e2a09f2a2b6c94e12eab
Vuizz <karam_theo@hotmail.com>
2022-08-07 00:46:29 +0300

    initial commit

--- static/Scripts/game.v4.js
+++ static/Scripts/game.v4.js
@@ -7,8 +7,8 @@ function expandA(){
     if (a == 1){
         document.getElementById("FAQ1").innerHTML = "What is the game?"
         document.getElementById('FAQ1').style.fontSize = '1.5rem'
-        document.getElementById("Faq1").style.backgroundColor = 'red'
-        document.getElementById("FAQ1").style.color = 'rgba(221,241,195,1)'
+        document.getElementById("Faq1").style.backgroundColor = '#a76571'
+        document.getElementById("FAQ1").style.color = 'black'
         return a = 0
     }
     else {
@@ -24,8 +24,8 @@ function expandB(){
     if (b == 1){
         document.getElementById("FAQ2").innerHTML = "How to play?"
         document.getElementById('FAQ2').style.fontSize = '1.5rem'
-        document.getElementById("Faq2").style.backgroundColor = 'blue'
-        document.getElementById("FAQ2").style.color = 'rgba(221,241,195,1)'
+        document.getElementById("Faq2").style.backgroundColor = '#565676'
+        document.getElementById("FAQ2").style.color = 'black'
 
         return b = 0
     }
@@ -42,8 +42,8 @@ function expandC(){
     if (c == 1){
         document.getElementById("FAQ3").innerHTML = "When game?"
         document.getElementById('FAQ3').style.fontSize = '1.5rem'
-        document.getElementById("Faq3").style.backgroundColor = 'green'
-        document.getElementById("FAQ3").style.color = 'rgba(221,241,195,1)'
+        document.getElementById("Faq3").style.backgroundColor = '#88d498'
+        document.getElementById("FAQ3").style.color = 'black'
 
         return c = 0
     }
@@ -61,8 +61,8 @@ function expandD(){
     if (d == 1){
         document.getElementById("FAQ4").innerHTML = "How to win?"
         document.getElementById('FAQ4').style.fontSize = '1.5rem'
-        document.getElementById("Faq4").style.backgroundColor = 'purple'
-        document.getElementById("FAQ4").style.color = 'rgba(221,241,195,1)'
+        document.getElementById("Faq4").style.backgroundColor = '#e2de84'
+        document.getElementById("FAQ4").style.color = 'black'
 
         return d = 0
     }
