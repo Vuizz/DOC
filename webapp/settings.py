5cc47ce4894974fe43a642e3a6b7b9188662b6bf
Vuizz <karam_theo@hotmail.com>
2022-10-03 16:51:35 +0300

    deploying to railway

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-48h163lehsx@43a7+j!733001o43k$uipl)mbt&lufqcrb9j#w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
