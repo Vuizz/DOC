560e5e1cd1ef387ebf7c6e46b789aa04a59418d9
Vuizz <karam_theo@hotmail.com>
2022-10-11 17:40:16 +0300

    updated mint js

--- static/Scripts/mintv4.js
+++ static/Scripts/mintv4.js
@@ -1,1 +1,50 @@
-var number=1;function Increment(){number<3&&(number+=1),2==number&&(img="visible",img2="hidden",img3="visible"),3==number&&(img3="visible",img2="visible",img="visible"),amnt=number}function Decrement(){number>1&&(number-=1),2==number&&(img="visible",img2="hidden",img3="visible"),1==number&&(img="hidden",img2="visible",img3="hidden"),amnt=number}function popmintscreen(){srcn="visible",cost=53*number,prce=`${cost} ADA`}function closemintscreen(){srcn="hidden"}img=document.getElementById("QuestionMark").style.visibility,img2=document.getElementById("QuestionMark2").style.visibility,img3=document.getElementById("QuestionMark3").style.visibility,amnt=document.getElementById("Amount").innerHTML,srcn=document.getElementById("MintScreen").style.visibility,prce=document.getElementById("mintCost").innerHTML;
+var number = 1;
+img = document.getElementById("QuestionMark").style.visibility
+img2 = document.getElementById("QuestionMark2").style.visibility
+img3 = document.getElementById("QuestionMark3").style.visibility
+amnt = document.getElementById("Amount").innerHTML
+srcn = document.getElementById('MintScreen').style.visibility
+prce = document.getElementById('mintCost').innerHTML
+function Increment(){
+    if (number < 3){
+    number += 1
+    }
+    if (number == 2){
+        img = "visible"
+        img2 = "hidden"
+        img3 = "visible"
+    }
+    if (number == 3){
+        img3 = "visible"
+        img2 = "visible"
+        img = "visible"
+    }
+    amnt = number
+}
+
+function Decrement(){
+    if (number > 1){
+    number -= 1
+    }
+    if (number == 2){
+        img = "visible"
+        img2 = "hidden"
+        img3 = "visible"
+    }
+    if (number == 1){
+        img = "hidden"
+        img2 = "visible"
+        img3 = "hidden"
+    }
+    amnt = number
+}
+
+function popmintscreen(){
+    srcn = 'visible'
+    cost = number * 53
+    prce = `${cost} ADA`
+}
+
+function closemintscreen(){
+    srcn = 'hidden'
+}
